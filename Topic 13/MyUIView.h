477d20a00574413a75e9349d4d93b4b08ea26c30
Lun Sovathana <sovathanalun@gmail.com>
2015-11-19 16:18:18 +0700

    test

---
+++ Topic 13/MyUIView.h
@@ -0,0 +1,13 @@
+//
+//  MyUIView.h
+//  Topic 13
+//
+//  Created by Lun Sovathana on 11/19/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyUIView : UIView<NSObject>
+
+@end
--- Topic 13/ViewController.m
+++ Topic 13/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MyUIView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // get parent point
+    int x = self.view.frame.size.width/2 - 45;
+    int y = self.view.frame.size.height/2 - 45;
+    
+    MyUIView *myView = [[MyUIView alloc] initWithFrame: CGRectMake(x, y, 90, 90)];
+    
+    // constrain
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(<=1)-[myView]-|"
+                                            options:NSLayoutFormatAlignAllCenterY
+                                            metrics:nil
+                                              views:NSDictionaryOfVariableBindings(self.view, myView)];
+    
+    [self.view addSubview:myView];
 }
 
 - (void)didReceiveMemoryWarning {
